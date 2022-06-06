e97e4dffa95abfed26a458e4c1bfe151758f2957
dav033 <87620700+dav033@users.noreply.github.com>
2022-06-06 00:34:55 -0500

    a

--- src/database.js
+++ src/database.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 // const url = process.env.MONGODB_URL
 //   ? process.env.MONGODB_URL
 //   : "mongodb://localhost/27017/hola";
-//const url =
-  "mongodb+srv://david:clavesegura03@cluster0.ic3ur.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const url = "mongodb://localhost/adios";
+const url =
+  "mongodb+srv://david1:clavesegura03@cluster0.fh0si84.mongodb.net/?retryWrites=true&w=majority";
+//const url = "mongodb://localhost/adios";
 
 mongoose.connect(url, {
   useNewUrlParser: true,
